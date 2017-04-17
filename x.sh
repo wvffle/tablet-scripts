cc254633faf0b2379685ce96b6dcf84a5bfbc37e
wvffle <casper@wvffle.net>
2017-04-17 10:52:23 +0200

    Update x.sh

--- x.sh
+++ x.sh
@@ -2,8 +2,6 @@
 
 while [ 1 -eq 1 ]
 do
-	sleep 1
-	[[ $(ps -u|grep -ci xinit\ --) -lt 2 ]] && {
-		xinit -- :0 -dpi 100 -sharevts vt0&
-	}
+  sleep 1
+  [[ $(ps -u|grep -ci xinit\ --) -lt 2 ]] && xinit -- :0 -dpi 100 -sharevts vt0&
 done

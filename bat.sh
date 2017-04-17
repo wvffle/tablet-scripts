c1256fc14bc71d2129373ad079adb5bf5626320e
wvffle <casper@wvffle.net>
2017-04-17 10:50:06 +0200

    bat.sh

---
+++ bat.sh
@@ -0,0 +1,15 @@
+charge=$(</sys/class/power_supply/battery/charge_now)
+capacity=$(</sys/class/power_supply/battery/capacity)
+case $1 in
+	"status")
+		[[ $charge -gt 0 ]] && echo charging
+		[[ $charge -gt 0 ]] || echo discharging
+		;;
+	"percent")
+		echo $capacity
+		;;
+	*)
+		res="$capacity%"
+		[[ $charge -gt 0 && $capacity -ne 100 ]] && res="+$res"
+		echo $res
+esac

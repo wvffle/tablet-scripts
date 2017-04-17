8f8c8c6d95d7f5114d422ad7c5f54eca5de2b9fa
wvffle <casper@wvffle.net>
2017-04-17 10:54:23 +0200

    Update brightness.sh

--- brightness.sh
+++ brightness.sh
@@ -1,18 +1,15 @@
-[[ $# -eq 0 ]] && {
-	cat /sys/class/backlight/panel/actual_brightness
-}
+#!/bin/sh
+
+[[ $# -eq 0 ]] && cat /sys/class/backlight/panel/actual_brightness
 [[ $# -gt 0 ]] && {
-	brightness=$1
-	[[ $1 == [0-9]* ]] || {
-		brightness=$(</sys/class/backlight/panel/actual_brightness)
-		[[ "$1" == "-" ]] && {
-			let "brightness-=16"
-		}
-		[[ "$1" == "+" ]] && {
-			let "brightness+=16"
-		}
-	}
-	[[ "$brightness" -lt 0 ]]   && brightness=0
-	[[ "$brightness" -gt 255 ]] && brightness=255
-	echo $brightness > /sys/class/backlight/panel/brightness
+  brightness=$1
+  [[ $1 == [0-9]* ]] || {
+    brightness=$(</sys/class/backlight/panel/actual_brightness)
+    [[ "$1" == "-" ]] && let "brightness-=16"
+    [[ "$1" == "+" ]] && let "brightness+=16"
+  }
+  
+  [[ "$brightness" -lt 0 ]]   && brightness=0
+  [[ "$brightness" -gt 255 ]] && brightness=255
+  echo $brightness > /sys/class/backlight/panel/brightness
 }
